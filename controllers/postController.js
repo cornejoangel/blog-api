bf51fe4da6fc3c46a7676efc996a68110475409b
Angel Cornejo <self.angelcornejo@gmail.com>
2022-06-30 10:56:52 -0700

    Adds controllers with dummy functions

---
+++ controllers/postController.js
@@ -0,0 +1,41 @@
+const Post = require('../models/post');
+
+// Display detail page for a post
+exports.post_detail = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DETAIL');
+};
+
+// Display list of posts
+exports.post_list = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER LIST');
+};
+
+// GET for creating Post
+exports.post_create_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER CREATE GET');
+};
+
+// POST for creating Post
+exports.post_create_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER CREATE POST');
+};
+
+// GET for deleting Post
+exports.post_delete_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DELETE GET');
+};
+
+// POST for deleting Post
+exports.post_delete_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DELETE POST');
+};
+
+// GET for updating Post
+exports.post_update_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER UPDATE GET');
+};
+
+// POST for updating Post
+exports.post_update_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER UPDATE POST');
+};
---
+++ controllers/userController.js
@@ -0,0 +1,41 @@
+const User = require('../models/user');
+
+// Display detail page for a user
+exports.user_detail = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DETAIL');
+};
+
+// Display list of users
+exports.user_list = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER LIST');
+};
+
+// GET for creating User
+exports.user_create_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER CREATE GET');
+};
+
+// POST for creating User
+exports.user_create_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER CREATE POST');
+};
+
+// GET for deleting User
+exports.user_delete_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DELETE GET');
+};
+
+// POST for deleting User
+exports.user_delete_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER DELETE POST');
+};
+
+// GET for updating User
+exports.user_update_get = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER UPDATE GET');
+};
+
+// POST for updating User
+exports.user_update_post = function (req, res, next) {
+  res.send('NOT IMPLEMENTED: USER UPDATE POST');
+};
