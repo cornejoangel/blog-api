bc0736ca08ffafe2b8fc03284a25af475d442962
Angel Cornejo <self.angelcornejo@gmail.com>
2022-06-30 11:16:58 -0700

    Fixes some copy/pasted innacuracies

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,40 +2,40 @@ const Comment = require('../models/comment');
 
 // Display detail page for a comment
 exports.comment_detail = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DETAIL');
+  res.send('NOT IMPLEMENTED: COMMENT DETAIL');
 };
 
 // Display list of comments
 exports.comment_list = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER LIST');
+  res.send('NOT IMPLEMENTED: COMMENT LIST');
 };
 
 // GET for creating Comment
 exports.comment_create_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER CREATE GET');
+  res.send('NOT IMPLEMENTED: COMMENT CREATE GET');
 };
 
 // POST for creating Comment
 exports.comment_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER CREATE POST');
+  res.send('NOT IMPLEMENTED: COMMENT CREATE POST');
 };
 
 // GET for deleting Comment
 exports.comment_delete_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DELETE GET');
+  res.send('NOT IMPLEMENTED: COMMENT DELETE GET');
 };
 
 // POST for deleting Comment
 exports.comment_delete_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DELETE POST');
+  res.send('NOT IMPLEMENTED: COMMENT DELETE POST');
 };
 
 // GET for updating Comment
 exports.comment_update_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER UPDATE GET');
+  res.send('NOT IMPLEMENTED: COMMENT UPDATE GET');
 };
 
 // POST for updating Comment
 exports.comment_update_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER UPDATE POST');
+  res.send('NOT IMPLEMENTED: COMMENT UPDATE POST');
 };
--- controllers/postController.js
+++ controllers/postController.js
@@ -2,40 +2,40 @@ const Post = require('../models/post');
 
 // Display detail page for a post
 exports.post_detail = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DETAIL');
+  res.send('NOT IMPLEMENTED: POST DETAIL');
 };
 
 // Display list of posts
 exports.post_list = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER LIST');
+  res.send('NOT IMPLEMENTED: POST LIST');
 };
 
 // GET for creating Post
 exports.post_create_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER CREATE GET');
+  res.send('NOT IMPLEMENTED: POST CREATE GET');
 };
 
 // POST for creating Post
 exports.post_create_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER CREATE POST');
+  res.send('NOT IMPLEMENTED: POST CREATE POST');
 };
 
 // GET for deleting Post
 exports.post_delete_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DELETE GET');
+  res.send('NOT IMPLEMENTED: POST DELETE GET');
 };
 
 // POST for deleting Post
 exports.post_delete_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER DELETE POST');
+  res.send('NOT IMPLEMENTED: POST DELETE POST');
 };
 
 // GET for updating Post
 exports.post_update_get = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER UPDATE GET');
+  res.send('NOT IMPLEMENTED: POST UPDATE GET');
 };
 
 // POST for updating Post
 exports.post_update_post = function (req, res, next) {
-  res.send('NOT IMPLEMENTED: USER UPDATE POST');
+  res.send('NOT IMPLEMENTED: POST UPDATE POST');
 };
