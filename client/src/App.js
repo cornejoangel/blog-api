d33f924d1c730c7bfbca83cfa204abf8e0ce0fc7
Angel Cornejo <self.angelcornejo@gmail.com>
2022-07-03 17:00:36 -0700

    Installs packages for React

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,41 @@
 import React, { useState } from 'react';
 
 const App = () => {
-  const [path, setPath] = useState("/api");
-  const [data, setData] = useState({ data: ""});
+  const [path, setPath] = useState('/api');
+  const [data, setData] = useState({ data: '' });
 
   const updatePath = (newPath) => {
     setPath(newPath);
-  }
+  };
 
   const getData = () => {
     fetch(path)
-      .then(result => result.json())
-      .then(body => setData(body))
+      .then((result) => result.json())
+      .then((body) => setData(body));
   };
 
   return (
     <div className="app">
       <h1>Blog</h1>
-      <input value={path} type="text" onChange={(e) => updatePath(e.target.value)} />
-      <button onClick={getData}>Get path data</button>
-      <div>
-        path: {path}
-      </div>
+      <input
+        value={path}
+        type="text"
+        onChange={(e) => updatePath(e.target.value)}
+      />
+      <button type="button" onClick={getData}>
+        Get path data
+      </button>
+      <div>path: {path}</div>
       <h2>Data:</h2>
       <div>
-        {Object.entries(data).map(([key, value]) =>
-          <p>{key} : {value} </p>
-        )}
+        {Object.entries(data).map(([key, value]) => (
+          <p>
+            {key} : {value}{' '}
+          </p>
+        ))}
       </div>
-
     </div>
-  )
+  );
 };
 
 export default App;
