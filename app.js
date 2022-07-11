369fbcff2017501a9a73324773356c6a1ac3a092
Angel Cornejo <self.angelcornejo@gmail.com>
2022-07-11 14:55:28 -0700

    Removes redundant code

--- app.js
+++ app.js
@@ -27,10 +27,8 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', indexRouter);
