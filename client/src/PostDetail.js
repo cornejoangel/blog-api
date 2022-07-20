ffb723408174985e37ad6c8d72ebbe63adae7c0f
Angel Cornejo <self.angelcornejo@gmail.com>
2022-07-20 12:02:02 -0700

    Post detail page displays comments

--- client/src/PostDetail.js
+++ client/src/PostDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import uniqid from 'uniqid';
 import Post from './components/Post';
 
 const PostDetail = () => {
@@ -19,6 +20,13 @@ const PostDetail = () => {
     <div>
       {post === null && <div>Loading...</div>}
       {post !== null && <Post post={post} />}
+      {post !== null && (
+        <ul>
+          {post.comments.map((comment) => (
+            <li key={uniqid()}>{comment.body}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
