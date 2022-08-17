08232a925a33e6d662dfb28f53ceb69b616e764d
Angel Cornejo <self.angelcornejo@gmail.com>
2022-08-17 11:04:45 -0700

    Removes delete GET route

--- routes/posts.js
+++ routes/posts.js
@@ -11,9 +11,6 @@ router.get('/create', postController.post_create_get);
 // POST request for creating Post.
 router.post('/create', postController.post_create_post);
 
-// GET request to delete Post.
-router.get('/:id/delete', postController.post_delete_get);
-
 // POST request to delete Post.
 router.post('/:id/delete', postController.post_delete_post);
 
