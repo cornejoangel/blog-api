d2eba6f77b325f3aae1252c2296c32c311109005
Angel Cornejo <self.angelcornejo@gmail.com>
2022-08-19 16:32:01 -0700

    Adds styles for posts

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,18 +1,22 @@
 import PropTypes from 'prop-types';
+import '../styles/Post.scss';
 
 const Post = (props) => {
   const { post } = props;
   const commentCount = post.comments?.length;
 
   return (
-    <div>
-      <h1>{post.title}</h1>
-      <div>{post.time_stamp_formatted}</div>
-      <p>{post.body}</p>
-      <div>
-        - {post.user.username} - {post.hidden} - {post.edited}
+    <div className="post">
+      <div className="author">{post.user.username}</div>
+      <div className="time-container">
+        <div className="time">{post.time_stamp_formatted}</div>
+        {post.edited && (
+          <div className="edited">(Edited: {post.edit_time_formatted})</div>
+        )}
       </div>
-      <div>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <div className="comment-count">
         {commentCount} {commentCount === 1 ? 'comment' : 'comments'}
       </div>
     </div>
