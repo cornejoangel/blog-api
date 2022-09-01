4a05d59ea5d01ea08a8bdc2c594022d2b3cc3183
Angel Cornejo <self.angelcornejo@gmail.com>
2022-08-31 19:24:23 -0700

    Adds mobile friendly styles for navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,35 +1,62 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../styles/Navbar.scss';
 
 const Navbar = (props) => {
   const { loggedIn, logout } = props;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    if (menuOpen) {
+      setMenuOpen(false);
+      return;
+    }
+    setMenuOpen(true);
+  };
 
   return (
     <nav>
-      <ul className="links">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/users">User List</Link>
-        </li>
-      </ul>
-      {!loggedIn && (
-        <ul className="auth">
+      <button
+        type="button"
+        className="menu-button"
+        onClick={() => toggleMenu()}
+      >
+        {menuOpen ? 'Close' : 'Menu'}
+      </button>
+      <div className={menuOpen ? 'nav-content open' : 'nav-content closed'}>
+        <ul className={menuOpen ? 'links open' : 'links closed'}>
           <li>
-            <Link to="/signup">Signup</Link>
+            <Link to="/" onClick={() => toggleMenu()}>
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/login">Login</Link>
+            <Link to="/users" onClick={() => toggleMenu()}>
+              User List
+            </Link>
           </li>
         </ul>
-      )}
-      {loggedIn && (
-        <button type="button" onClick={(e) => logout(e)}>
-          Log out
-        </button>
-      )}
+        {!loggedIn && (
+          <ul className={menuOpen ? 'auth open' : 'auth closed'}>
+            <li>
+              <Link to="/signup" onClick={() => toggleMenu()}>
+                Signup
+              </Link>
+            </li>
+            <li>
+              <Link to="/login" onClick={() => toggleMenu()}>
+                Login
+              </Link>
+            </li>
+          </ul>
+        )}
+        {loggedIn && (
+          <button type="button" onClick={(e) => logout(e)}>
+            Log out
+          </button>
+        )}
+      </div>
     </nav>
   );
 };
